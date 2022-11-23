0196a601b33c683a91217d3ee754fc83b6f91f0f
xDejw <dafik2001@gmail.com>
2022-11-23 14:01:35 +0100

    'synchronised'

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/public'));
+
+
+const userRouter = require("./routes/user");
+const noteRouter = require("./routes/note");
+const usernoteRouter = require("./routes/usernote");
+
+app.use(userRouter);
+app.use(noteRouter);
+app.use(usernoteRouter);
 
 app.get('/', (req, res) => {
     res.render('index', {});
-});
-
-app.get('pages/notes/add-invalid', (req, res) => {
-    res.render('pages/notes/add-invalid', {});
 });
 
 app.listen(3000, () => {
---
+++ controllers/noteController.js
@@ -0,0 +1,11 @@
+exports.showNotes = (req, res, next) => {
+    res.render('pages/users/list');
+}
+
+exports.showNotes = (req, res, next) => {
+    res.render('pages/users/list');
+}
+
+exports.showNotes = (req, res, next) => {
+    res.render('pages/users/list');
+}
